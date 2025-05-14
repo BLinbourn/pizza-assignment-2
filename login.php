98df287c68c69b8b50618bfb460efaaf8c05251d
Bethan Linbourn <77588933+TheBlueAttack@users.noreply.github.com>
2025-05-14 23:47:33 +0100

    Create the login page

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div> Log in here </div>
+    <label>Username</label>
+    <input type="text" id="username" name="username">
+    <label>Password</label>
+    <input type="text" id="password" name="passsword">
+    <input type="submit" value="Submit">
+</body>
+</html>
